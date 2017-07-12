6c459754add850cf117995fcbd480144be3e257f
14120 <>
2017-07-12 15:54:16 +0900

    firstCommit

---
+++ fileSearch/frmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace fileSearch
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView = new fileSearch.DoubleBufferListView();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Location = new System.Drawing.Point(12, 384);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(787, 21);
+            this.txtSearch.TabIndex = 10;
+            this.txtSearch.Text = "*.exe";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(803, 359);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 22);
+            this.btnBrowse.TabIndex = 9;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(803, 382);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 22);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtDir
+            // 
+            this.txtDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDir.Location = new System.Drawing.Point(12, 360);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(787, 21);
+            this.txtDir.TabIndex = 7;
+            this.txtDir.Text = "C:\\Users\\";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(887, 24);
+            this.menuStrip.TabIndex = 11;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Location = new System.Drawing.Point(12, 27);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(865, 320);
+            this.listView.TabIndex = 12;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView_ColumnClick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 414);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private DoubleBufferListView listView;
+    }
+}
+
---
+++ fileSearch/frmMain.cs
@@ -0,0 +1,307 @@
+﻿#region Namespace
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+#endregion
+
+namespace fileSearch
+{
+    public partial class frmMain : Form
+    {
+
+        #region Declare
+        private readonly ListViewColumnSorter lv_sorter = new ListViewColumnSorter();
+        private SaveFileDialog sfd = new SaveFileDialog();
+        private FolderBrowserDialog fbd = new FolderBrowserDialog();
+        private volatile bool _stop = false;
+        private int oldWidth = 0;
+        #endregion
+
+        #region Init
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            listView.FullRowSelect = true;
+            listView.View = View.Details;
+            listView.Columns.Add("Path", 500);
+            listView.Columns.Add("Cert", 150);
+            oldWidth = Width;
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            try
+            {
+                listView.Columns[0].Width -= oldWidth - Width;
+                oldWidth = Width;
+            }
+            catch
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region Buttons
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (btnSearch.Text == "Search")
+            {
+                _stop = false;
+                btnText("Stop");
+                listView.Items.Clear();
+                Thread t = new Thread(() => Search(txtDir.Text, txtSearch.Text));
+                t.Start();
+            }
+            else
+            {
+                _stop = true;
+            }
+            
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtDir.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnText(string text)
+        {
+            Invoke(new MethodInvoker(
+                () =>
+                {
+                    btnSearch.Text = text;
+                }));
+        }
+        #endregion
+
+        #region listView
+        private void listView_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            var lv = (ListView)sender;
+            lv.ListViewItemSorter = lv_sorter;
+
+            if (e.Column != lv_sorter.SortColumn)
+            {
+                lv_sorter.SortColumn = e.Column;
+                lv_sorter.Order = SortOrder.Ascending;
+            }
+            else
+                lv_sorter.Order = lv_sorter.Order == SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
+
+            lv.Sort();
+        }
+
+        private delegate void addToListViewDelegate(ListView lv, ListViewItem lvi);
+        private void addItemToListView(ListView lv, ListViewItem lvi)
+        {
+            if (lv.InvokeRequired)
+            {
+                Invoke(new addToListViewDelegate(addItemToListView), new object[] { lv, lvi });
+                return;
+            }
+            lv.Items.Add(lvi);
+        }
+        #endregion
+
+        #region saveFile
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sfd.InitialDirectory = Environment.CurrentDirectory;
+            sfd.Filter = "Text Files (*.txt)|*.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (var tw = new StreamWriter(new FileStream(sfd.FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite), Encoding.Default))
+                {
+                    foreach (ListViewItem lvi in listView.Items)
+                    {
+                        tw.WriteLine(lvi.Text + ";" + lvi.SubItems[1].Text + ";" + lvi.SubItems[2].Text);
+                    }
+                }
+            }
+        }
+        #endregion
+        
+        #region Search
+
+        private void Search(string path, string exts)
+        {
+            foreach (string file in GetFiles(path, exts))
+            {
+                if (_stop) break;
+                else
+                {
+                    string[] rows = { file, checkSignature(file).ToString() };
+                    ListViewItem lvi = new ListViewItem(rows);
+                    addItemToListView(listView, lvi);
+                }
+            }
+            btnText("Search");
+            return;
+        }
+
+        private IEnumerable<string> GetFiles(string path, string exts)
+        {
+            Queue<string> q = new Queue<string>();
+            q.Enqueue(path);
+            while (q.Count > 0)
+            {
+                path = q.Dequeue();
+                try
+                {
+                    foreach (string subDir in Directory.GetDirectories(path))
+                    {
+                        q.Enqueue(subDir);
+                    }
+                }
+                catch
+                {
+                   
+                }
+                string[] files = null;
+                try
+                {
+                    files = Directory.GetFiles(path, exts);
+                }
+                catch
+                {
+                    
+                }
+                if (files != null)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        yield return files[i];
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Signature
+        private bool checkSignature(string fileName)
+        {
+            X509Chain cert_chain = new X509Chain();
+            X509Certificate2 cert = default(X509Certificate2);
+            bool is_chain_valid = false;
+            try
+            {
+                X509Certificate signer = X509Certificate.CreateFromSignedFile(fileName);
+                cert = new X509Certificate2(signer);
+            }
+            catch
+            {
+                return is_chain_valid;
+            }
+
+            cert_chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+            cert_chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+            cert_chain.ChainPolicy.UrlRetrieval‎Timeout = new TimeSpan(0, 1, 0);
+            cert_chain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+            is_chain_valid = cert_chain.Build(cert);
+
+            if (is_chain_valid)
+            {
+                return is_chain_valid;
+            }
+            else
+            {
+                return is_chain_valid;
+            }
+        }
+        #endregion
+
+
+    }
+
+    #region Sorter
+    public sealed class ListViewColumnSorter : IComparer
+    {
+        private int column_to_sort;
+        private SortOrder order_of_sort;
+        public ListViewColumnSorter()
+        {
+            column_to_sort = 0;
+            order_of_sort = SortOrder.Ascending;
+        }
+        public int Compare(object x, object y)
+        {
+            int compare_result = 0;
+            ListViewItem lv_x, lv_y;
+            lv_x = x as ListViewItem;
+            lv_y = y as ListViewItem;
+
+            if (lv_x != lv_y)
+            {
+                int int_lv_x, int_lv_y;
+                Int32.TryParse(lv_x.SubItems[column_to_sort].Text, out int_lv_x);
+                Int32.TryParse(lv_y.SubItems[column_to_sort].Text, out int_lv_y);
+
+                if (int_lv_x > 0 && int_lv_y > 0)
+                    compare_result = Decimal.Compare(int_lv_x, int_lv_y);
+                else
+                    compare_result = String.CompareOrdinal(lv_x.SubItems[column_to_sort].Text, lv_y.SubItems[column_to_sort].Text);
+            }
+
+            if (order_of_sort == SortOrder.Ascending)
+                return compare_result;
+
+            return -compare_result;
+        }
+
+        public int SortColumn
+        {
+            set
+            {
+                column_to_sort = value;
+            }
+            get
+            {
+                return column_to_sort;
+            }
+        }
+        public SortOrder Order
+        {
+            set
+            {
+                order_of_sort = value;
+            }
+            get
+            {
+                return order_of_sort;
+            }
+        }
+    }
+    #endregion
+
+    #region DoubleBuffer
+    public class DoubleBufferListView : ListView
+    {
+        public DoubleBufferListView()
+        {
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.EnableNotifyMessage, true);
+        }
+        protected override void OnNotifyMessage(Message m)
+        {
+            if (m.Msg != 0x14)
+            {
+                base.OnNotifyMessage(m);
+            }
+        }
+    }
+    #endregion
+}
